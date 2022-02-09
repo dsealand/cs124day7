f233ca0a89c891aeac012ed1ff4da2f37c7389bf
Daniel Sealand <daniel.c.sealand@gmail.com>
2022-02-09 13:43:25 -0800

    complete

--- src/index.js
+++ src/index.js
@@ -4,11 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const data = [
+    {
+        id: 512,
+        name: "Neil Rhodes",
+        email: "rhodes@hmc.edu",
+        phone: "(909) 555-1212"
+    },
+    {
+        id: 787,
+        name: "Barack Obama",
+        email: "ex-prez@whitehouse.gov",
+        phone: "(312) 555-1212"
+    }
+];
+
+const list = data.map(e => (<div key={e.id}><span className="bold">{e.name}</span> <span className="mono">{e.email}</span>
+    <span>{e.phone}</span></div>))
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    list,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
